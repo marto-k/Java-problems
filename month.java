85044668864dcd893f312d9542fceec718a59334
martin <martow17@gmail.com>
2016-02-03 08:51:24 +0300

    add git

---
+++ month.java
@@ -0,0 +1,55 @@
+Using Java, declare an int named month whose value represents a month. Your code should displays the name of the month, based on the value of month, using the switch statement.
+
+public class month{
+
+public static void main { []args
+ 
+ int Month= 0;
+ int month = input.method
+ switch (month){
+ 
+ case 1: input.method("January");
+ break ;
+ 
+ case 2: input.method="February";
+ break ;
+ 
+ case 3: input.method="March";
+ break ;
+ 
+ case 4: input.method="April";
+ break ;
+ 
+ case 5: input.method("May");
+ break ;
+ 
+ case 6: input.method("June");
+ break ;
+ 
+ case 7: input.method("July");
+ break ;
+ 
+ case 8: input.method("August");
+ break ;
+ 
+ case 9: input.method("September");
+ break ;
+ 
+ case 10: input.method="October");
+ break ;
+ 
+ case 11: input.method="November";
+break ;
+
+ case 12: input.method="December";
+ break ;
+ 
+ default:  input.method = "Invalid month";
+                     break;
+ }
+   }
+     }
+
+  
+  
+  
